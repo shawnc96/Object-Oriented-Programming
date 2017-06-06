1c564c7964e903fb2e82f8937c4603f1108da0d0
Shawn Clarke <shawn.a.c96@hotmail.com>
2017-06-06 17:23:19 -0400

    Add files via upload

---
+++ Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VenueSort
+{
+    class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipCode { get; set; }
+
+        //Constructor
+        public Address(string Street, string City, string State, int Zip)
+        {
+            this.streetAddress = Street;
+            this.city = City;
+            this.state = State;
+            this.zipCode = Zip;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(this.streetAddress);
+            Console.WriteLine(this.city + ", " + this.state);
+            Console.WriteLine(this.zipCode);
+        }
+    }
+}
